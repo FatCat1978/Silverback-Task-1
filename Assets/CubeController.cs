2070fb12917d3c47e2a1638cb1119d5ee99e073d
FatCat1978 <Pacmandevil1@gmail.com>
2022-06-28 00:16:48 -0300

    More updates to the cube controller, redo the cube prefab.

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
-	[SerializeField] List<GameObject> allCubes; 
-	private List<GameObject> inactiveCubes;
-	private List<GameObject> activeCubes;
+	[SerializeField] public List<GameObject> allCubes;  
+	private List<GameObject> inactiveCubes = new List<GameObject>(); //these need to be initialized, allcubes doesn't
+	private List<GameObject> activeCubes = new List<GameObject>();
 
     [Tooltip("It wasn't quite clear if you wanted the cube selection to be exclusive (ae, normally one able to be selected at a time), or inclusive, (ae, click to toggle individual), so I just supported both, shouldn't take any extra time.")]
 	[SerializeField] bool exclusiveCubeSelection = false; //false by default though
@@ -15,13 +15,50 @@ public class CubeController : MonoBehaviour
 
     private void Start() //called once!
     {
-     //we iterate over every cube in AllCubes, and add them to inactive or active cubes depending on whether they're active or inactive according to their individual controller.   
+     //we iterate over every cube in AllCubes, and add them to inactive or active cubes depending on whether
+	 //they're active or inactive according to their individual controller.   
+
+		foreach (GameObject cube in allCubes)
+        {
+			//we check for the Cubehandler
+			CubeHandler CH = cube.GetComponent<CubeHandler>();
+			if(CH == null) //not a valid cube!
+            {
+				print("Invalid Cube: " + cube.name + " found. deleting it!"); //soft sanity check, considering the cubes get auto added.
+				allCubes.Remove(cube);
+				GameObject.Destroy(cube);
+            }
+			else
+            {
+				if(CH.activeSelection)
+					activeCubes.Add(cube);
+				else
+					inactiveCubes.Add(cube);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
 	{
-		
+		if (Input.GetMouseButtonDown(0)) //if we're clicking
+		{
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //make a ray, cast it out
+			RaycastHit hit;
+			// Casts the ray and get the first game object hit
+			Physics.Raycast(ray, out hit);
+
+			GameObject foundObject = hit.rigidbody.gameObject;
+
+			Debug.Log("This hit" + foundObject.name);
+			if (foundObject != null) //sanity check
+            {
+				if(allCubes.Contains(foundObject))
+                {
+					ToggleCubeActivation(foundObject);
+                }
+            }
+		}
 	}
 
 	void ToggleCubeActivation(GameObject toToggleCube)
--- Assets/CubeHandler.cs
+++ Assets/CubeHandler.cs
@@ -11,23 +11,28 @@ public class CubeHandler : MonoBehaviour
 	[SerializeField] Material selectedMaterial; //what do we look like when selected? red.
 	[SerializeField] Material inactiveMaterial; //what about when we're inactive??
 
-	[SerializeField] bool active = false;
+	[SerializeField] public bool activeSelection = false; //needs to be public. cubecontroller checks this to organize us properly.
 
 
 	private Rigidbody rigidBody;
 	private MeshRenderer Renderer;
-
+	private CubeController controller;
 	// Start is called before the first frame update
 	void Start()
 	{
 		Renderer = GetComponent<MeshRenderer>();
 		SetMaterialToActiveState();
-		//get the main cube controller
+		//get the main cube controller, add ourselves to it if we aren't in there already.
+
+		controller = GameObject.Find("CubeManager").GetComponent<CubeController>(); //Just so we have it.
+		if(!controller.allCubes.Contains(this.gameObject))
+			controller.allCubes.Add(this.gameObject);
+
 	}
 
 	private void SetMaterialToActiveState()
 	{
-		if(active)
+		if(activeSelection)
 			Renderer.material = selectedMaterial;
 		else
 			Renderer.material = inactiveMaterial;
@@ -37,8 +42,11 @@ public class CubeHandler : MonoBehaviour
     //Editor Fluff
     private void OnValidate()
     {
+		if (!Renderer)
+			Renderer = GetComponent<MeshRenderer>();
 		SetMaterialToActiveState();
-    }
+
+	}
 
     // Update is called once per frame
     void Update()
