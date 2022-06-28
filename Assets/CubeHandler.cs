cbb883e312630f8ab140fef06fc1ecc5d55cd6d5
FatCat1978 <Pacmandevil1@gmail.com>
2022-06-28 00:49:58 -0300

    change the prefab, update the scene a bit. final touches, basically.

--- Assets/CubeHandler.cs
+++ Assets/CubeHandler.cs
@@ -14,6 +14,8 @@ public class CubeHandler : MonoBehaviour
 	[SerializeField] public bool activeSelection = false; //needs to be public. cubecontroller checks this to organize us properly.
 
 
+	Vector3 originalPosition; // where the box is returned to after falling for 7 seconds.
+
 	private Rigidbody rigidBody;
 	private MeshRenderer Renderer;
 	private CubeController controller;
@@ -22,11 +24,15 @@ public class CubeHandler : MonoBehaviour
 	{
 		Renderer = GetComponent<MeshRenderer>();
 		SetMaterialToActiveState();
+		rigidBody = GetComponent<Rigidbody>();
 		//get the main cube controller, add ourselves to it if we aren't in there already.
 
 		controller = GameObject.Find("CubeManager").GetComponent<CubeController>(); //Just so we have it.
 		if(!controller.allCubes.Contains(this.gameObject))
 			controller.allCubes.Add(this.gameObject);
+
+		originalPosition = this.transform.position;
+		originalPosition.y += 5;
 
 	}
 
@@ -48,9 +54,26 @@ public class CubeHandler : MonoBehaviour
 
 	}
 
-    // Update is called once per frame
+	// Update is called once per frame, it's used to determine when the cube's fallen off the board, and return it after 7 seconds. pretty simple stuff
+
+	private bool falling = false;
+	private float secondsSinceFall = 0;
     void Update()
 	{
+		if(transform.position.y < 0 && !falling)
+        {
+			falling = true;
+        }
+		if (falling)
+			secondsSinceFall += Time.deltaTime;
+
+		if(secondsSinceFall >= 7)
+        {
+			falling = false;
+			transform.position = originalPosition;
+			rigidBody.velocity = Vector3.zero; //reset it's velocity, otherwise it'll just phase through the plane.
+			secondsSinceFall = 0;
+        }
 		
 	}
 }
