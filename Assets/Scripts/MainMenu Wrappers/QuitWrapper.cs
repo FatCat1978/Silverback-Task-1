9a047ae5ac6dd08ababaf9dbd80e4b5397924efd
FatCat1978 <Pacmandevil1@gmail.com>
2022-06-27 23:11:55 -0300

    Quit button implemented. little bit of janky flair for making it work in the editor.
    changed the name and moved a few things around, too. clean.
    
    As an aside, you may note my other repos don't have nearly as many commits. this is mostly because they're for myself, and any issues that a lack of consistent and well documented commits may cause is future me's problem, and I hate that guy.
    
    For cases where I'm working on a team, it's not just future me's problem, so I can't really morally justify getting away with that. I've had to fix enough repos already.

---
+++ Assets/Scripts/MainMenu Wrappers/QuitWrapper.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class QuitWrapper : MonoBehaviour
+{
+  //doesn't do much, but it has a completely different goal when compared to SceneManagerWrapper, so it's a different class.
+  //in a full game/application, I would make this do sanity checks to make sure everything is saved properly, but for this..
+  //no need!
+
+    //still public for the button
+    public void StartGameQuit()
+    {
+        print("Quitting game.");
+        Application.Quit(); //as far as I know, only works inside a compiled exe.
+        EditorApplication.ExecuteMenuItem("Edit/Play"); //for the editor, then.
+    }
+
+}
